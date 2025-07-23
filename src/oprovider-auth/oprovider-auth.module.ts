5d88bab948c71d72397237bd2007e031e30e43c7
José <jcuencacoronado@gmail.com>
2025-07-23 01:08:57 -0400

    Correcion keycloak module connect

--- src/oprovider-auth/oprovider-auth.module.ts
+++ src/oprovider-auth/oprovider-auth.module.ts
@@ -6,9 +6,8 @@ import { KeycloakConnectModule } from 'nest-keycloak-connect';
 import { KeycloakEnvs } from 'src/config';
 @Module({
   imports: [KeycloakConnectModule.register({
-    // authServerUrl: KeycloakEnvs.authServerUrl,
+    authServerUrl: KeycloakEnvs.authServerUrl,
     // authServerUrl: "http://192.168.1.100:8080",
-    authServerUrl: "http://192.168.1.100:8080",
     realm: KeycloakEnvs.realm,
     clientId: KeycloakEnvs.clientId,
     secret: KeycloakEnvs.secret,
